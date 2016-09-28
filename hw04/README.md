10ad3269caacd44d49c7e9355d36b31e18a102f1
Boyu Zhang <zbyzby2012@gmail.com>
2016-09-27 20:19:40 -0400

    commit hw04

---
+++ hw04/README.md
@@ -0,0 +1,21 @@
+GPIO via mmap
+Please use following command to compile C files
+cc -O3 -g  -o gpioThru gpioThru.c
+cc -O3 -g  -o gpioToggle gpioToggle.c
+
+gpioThru
+
+input bottoms: 	  P9_16; P9_17
+LED output:       P9_12; P9_15
+Press any of bottoms, corresponding led will be turned on 
+
+gpioToggle
+input: P9_42
+output: P9_21
+
+MatrixLED
+1. The connect() function of matrixLED.js send different messages to the bone. BoneServer.js listens socket 9090 for receiving messages. Once it receives any messages, it will perform according operations on the bone like shell commands.
+2. When an “LED” is clicked the LEDclick function will be called. Value in array disp[] will be updated and will be sent to the bone.
+3. The entry “background-color” in the class “on” is used to color the LED.
+4. In my version of marixLED.js, I have two functions “leftLEDclick” and “rightLEDclick” which are similar to the function “LEDclick” in the example marixLED.js file, but “leftLEDclick” only turns on green LED while “rightLEDclick” turns on red LED. In the html, I add “onclick="leftLEDclick(' + i + ',' + j + ')" oncontextmenu="rightLEDclick(' + i + ',' + j + ')”;” to each “LED” block. When the user left clicks the block, green led will be turned on and when he right clicks the block, red led will be turned on. Disp[] and disp[]  will sent between bone and browser.
+5. No need to change boneSever.js 
---
+++ hw04/gpioToggle.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include "beaglebone_gpio.h"
+
+int keepgoing = 1;
+void signal_handler(int sig);
+
+void signal_handler(int sig)
+{
+	printf( "\nCtrl-C pressed, cleaning up and exiting...\n" );
+	exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    volatile void *gpio0_addr;
+    volatile unsigned int *gpio0_oe_addr;
+    volatile unsigned int *gpio0_setdataout_addr;
+    volatile unsigned int *gpio0_cleardataout_addr;
+    volatile unsigned int *gpio0_datain_addr;
+    
+    volatile void *gpio1_addr;
+    volatile unsigned int *gpio1_oe_addr;
+    volatile unsigned int *gpio1_setdataout_addr;
+    volatile unsigned int *gpio1_cleardataout_addr;
+    volatile unsigned int *gpio1_datain_addr;
+    
+    unsigned int reg;
+    unsigned int reg1;
+	signal(SIGINT, signal_handler);
+    int fd = open("/dev/mem", O_RDWR);
+    printf("Mapping %X - %X (size: %X)\n", GPIO1_START_ADDR, GPIO1_END_ADDR, GPIO1_SIZE);
+    gpio1_addr = mmap(0, GPIO1_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO1_START_ADDR);
+
+    gpio1_oe_addr           = gpio1_addr + GPIO_OE;
+    gpio1_setdataout_addr   = gpio1_addr + GPIO_SETDATAOUT;
+    gpio1_cleardataout_addr = gpio1_addr + GPIO_CLEARDATAOUT;
+    gpio1_datain_addr       = gpio1_addr + GPIO_DATAIN;
+
+    if(gpio1_addr == MAP_FAILED) {
+        printf("Unable to map GPIO1\n");
+        exit(1);
+    }
+    printf("GPIO1 mapped to %p\n", gpio1_addr);
+    printf("GPIO1 OE mapped to %p\n", gpio1_oe_addr);
+    printf("GPIO1 SETDATAOUTADDR mapped to %p\n", gpio1_setdataout_addr);
+    printf("GPIO1 CLEARDATAOUT mapped to %p\n", gpio1_cleardataout_addr);
+    printf("GPIO1 DATAIN mapped to %p\n", gpio1_datain_addr);
+    printf("Mapping %X - %X (size: %X)\n", GPIO0_START_ADDR, GPIO1_END_ADDR, GPIO0_SIZE);
+    gpio0_addr = mmap(0, GPIO0_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO0_START_ADDR);
+
+    gpio0_oe_addr           = gpio0_addr + GPIO_OE;
+    gpio0_setdataout_addr   = gpio0_addr + GPIO_SETDATAOUT;
+    gpio0_cleardataout_addr = gpio0_addr + GPIO_CLEARDATAOUT;
+    gpio0_datain_addr       = gpio0_addr + GPIO_DATAIN;
+
+    if(gpio0_addr == MAP_FAILED) {
+        printf("Unable to map GPIO0\n");
+        exit(1);
+    }
+    printf("GPIO0 mapped to %p\n", gpio0_addr);
+    printf("GPIO0 OE mapped to %p\n", gpio0_oe_addr);
+    printf("GPIO0 SETDATAOUTADDR mapped to %p\n", gpio0_setdataout_addr);
+    printf("GPIO0 CLEARDATAOUT mapped to %p\n", gpio0_cleardataout_addr);
+    printf("GPIO0 DATAIN mapped to %p\n", gpio0_datain_addr);
+    reg = *gpio1_oe_addr;
+    printf("GPIO1 configuration: %X\n", reg);
+    reg &= ~GPIO_60;
+    reg &= ~GPIO_48;
+    *gpio1_oe_addr = reg;
+    printf("GPIO1 configuration: %X\n", reg);
+    *gpio0_oe_addr =0;
+
+    while(keepgoing) {
+        reg = *gpio0_datain_addr;
+        reg &= GPIO_05;
+        if (reg) {
+        *gpio1_setdataout_addr = GPIO_60;}
+        else {
+        *gpio1_cleardataout_addr = GPIO_60;
+        }
+        
+        reg1 = *gpio1_datain_addr;
+        reg1 &= GPIO_51;
+        if (reg1) {
+        *gpio1_setdataout_addr = GPIO_48;}
+        else {
+        *gpio1_cleardataout_addr = GPIO_48;
+        }
+    }
+    munmap((void *)gpio1_addr, GPIO1_SIZE);
+    close(fd);
+    return 0;
+}
